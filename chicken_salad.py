4b5c8a88958ee7797071552b7e0894a6c51f4cad
j39m <hikaph@gmail.com>
2014-12-19 12:16:40 -0800

    string muncher

--- chicken_salad.py
+++ chicken_salad.py
@@ -9,3 +9,25 @@ def expm(base, exponent, mod):
     base %= mod
     exponent -= 1
   return base
+
+# remove every other character per line in a text file 
+# of particular ascii art. 
+
+import codecs 
+def sat(filename): 
+  fp = codecs.open(filename, "r", encoding = "utf-8")
+  out = codecs.open("satsuki-out", "w", encoding = "utf-8")
+  for line in fp: 
+    strout = " "
+    i = 0
+    while (i < len(line)): 
+      if (i % 3 == 1) or (i%3 == 2): 
+        strout += line[i]
+      i += 1
+    if (strout[-1] != '\n'): 
+      strout += '\n'
+    out.write(strout)
+
+  return 
+
+sat("satsuki")
