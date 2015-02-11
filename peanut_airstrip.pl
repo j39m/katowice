fda2932462a76792725133d35183e30d62ac98c1
j39m <hikaph@gmail.com>
2015-02-11 03:14:41 -0800

    better handling of 3xx http codes, add option to sort titchy files into a separate place

--- peanut_airstrip.pl
+++ peanut_airstrip.pl
@@ -7,7 +7,7 @@ use strict;
 use warnings; 
 
 if (@ARGV < 1) { 
-  print "usage: xtr.pl <input file>\n" and exit 1; 
+  print "usage: xtr.pl <input file> <filter image type> <minimum width> <minimum height>\n" and exit 1; 
 
 } 
 
@@ -19,17 +19,37 @@ if (defined $ARGV[1]) {
   $extension = "jpg"
 } 
 
+# work with file sizes
+my $size1 = 442; 
+my $size2 = 442; 
+if ((defined $ARGV[2]) and (defined $ARGV[3])) { 
+  if (($ARGV[2] =~ m/[^0-9]+/) or ($ARGV[3] =~ m/[^0-9]+/)) { 
+    printf STDERR "Incoherent size arguments $ARGV[2] and $ARGV[3];" .
+      " defaulting to 442 by 442\n"; 
+  } else { 
+    $size1 = $ARGV[2]; 
+    $size2 = $ARGV[3]; 
+  } 
+}
+
 # else proceed to parse webpage. 
 
 open my $oz, "<", $ARGV[0] 
   or die "couldn't open '$ARGV[0]!'\n"; 
 
+my $name_prefix = "hahahohohehe_-_"; # change this for naming
 my $counter = 10; 
+
+# set params for files to be discarded (too small)
+my $discard = "discard_-_"; 
+my $discard_counter = 10; 
+my $discard_fullname = ""; 
 
 while (my $line = <$oz>) { 
   
   my @stuff = split(" ", $line); # shot in the dark instead of 
   # multiple capture groups, eurgh
+  my $fullname = ""; # to be determined during run
 
   foreach (@stuff) { 
     if ($_ =~ m/(http:\/\/[^ ]+\.$extension)/) { 
@@ -37,10 +57,26 @@ while (my $line = <$oz>) {
       my $tama = $1; 
       # "&amp;" = "&"
       $tama =~ s/&amp;/&/g; 
+      
       # GET
       print "getting $tama\n"; 
-      system("curl -s -o hahahohohehe_-_$counter.$extension '$tama'"); 
-      $counter++; 
+      $fullname = $name_prefix . $counter . "." . $extension; 
+      system("curl -L -s -o $fullname '$tama'"); 
+      
+      # check if file is good? 
+      my $identifier = `/usr/bin/identify $fullname`; 
+      if ($identifier =~ m/ ([0-9]+)x([0-9]+) /) { 
+        if (($1 < $size1) and ($2 < $size2)) { 
+          $discard_fullname = $discard . $discard_counter . "." . $extension; 
+          system("mv -fv $fullname $discard_fullname"); 
+          $discard_counter++; 
+        } else { 
+          $counter++; 
+        } 
+      } else { 
+        print STDERR "couldn't get size for $fullname!\n"; 
+        print STDERR "identify outputs: $identifier\n"; 
+      }
     } 
   } 
 
