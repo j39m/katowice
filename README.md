ff1895a3ad053c3133788b88db3e4297bbd44a11
j39m <hikaph@gmail.com>
2015-02-27 02:58:22 -0800

    unskipper is a Python script that undoes all skip counts in your Quod Libet library

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 ... is a potpourri of projects I cobble together in my spare time. They have no coherence, no point, no taste. What else could they be for but to stroke my ego? 
 
+## unskipper 
+... is a Python script that prunes skipcounts from your Quod Libet library. It's a selfish tic of mine that I don't like seeing nonzero skipcounts on my songs because usually they happen by accident, and of course increase proportionally with the number of times I listen to a song (probably nonlinearly). It'll zero out all skipcounts in your library by popping the associated key from the dictionaries in your library and write the results back to disk. 
+
 ## emuberry_elgar & durian_elgar
 ... are a pair of Perl scripts that do "absolutely nothing". emuberry_elgar will take a sequence of entirely lowercase words (no punctuation; delimit with the string "STOP") and print a transformation of each word to stdout, for which the format will be "(int wordlength).(cryptographic hash of the word)," for which the dot is a literal period (not just silent concatenation). The particular hash defaults to md5, but can be changed by tweaking the appropriate parameters in both scripts (they must match for the scripts to work - in particular, be sure that you have the correct $hashlen variable in the latter, should you change to a different hash). durian_elgar will take in a message having the same form of output as emuberry_elgar and print the message that corresponds to the input hash. Beware: there is absolutely no memoization (to be implemented), no rainbow tables, no nothing - so durian_elgar will run horrendously slow in practice, unless u sv rly trn ct ea ch wrd to be de cd ed. 
 
---
+++ unskipper.py
@@ -0,0 +1,91 @@
+#! /usr/bin/env python2 
+
+# unskipper.py will prune all skipcounts from your Quod Libet library; 
+# the resulting lack of '~#skipcount' in your per-song entries will all
+# be interpreted by QL as being skipcount 0. 
+
+
+# load the pickled library. returns the songs pickle if it can find it, 
+# None if it cannot. 
+def load_library(): 
+
+  try: 
+    songs = pickle.load ( open ( path_to_songs, 'r' ) )
+  except IOError: 
+    print ("Error loading songs pickle. Does '%s' exist?" % path_to_songs)
+    return None 
+
+  return songs
+
+
+# in case we muck things up, have a backup on hand 
+def backup_library(): 
+
+  import shutil
+
+  path_to_bkup =  os.getenv('HOME') + "/.quodlibet/unpruned_songs"
+
+  shutil.copy(path_to_songs, path_to_bkup) 
+  print ("Current library state copied to %s" % path_to_bkup) 
+
+  return 
+
+
+# weed out skipcounts wherever they may be
+def prune_skips(song_pickle): 
+  
+  found_skips = False # make me true if we need to prune anything
+
+  for song in song_pickle: 
+    if ('~#skipcount' in song): 
+      found_skips = True
+      try: 
+        print ("prune: %d skips on '%s'" % (song.pop('~#skipcount'), song['title'] ) )
+      except KeyError:  # if song is untitled???
+        print ("prune: %d skips on untitled song" % song.pop('~#skipcount') )
+
+  if (not found_skips): 
+    print ("No skips pruned.") 
+
+  # write the finished pickle down 
+  try: 
+    pickle.dump(song_pickle, open(path_to_songs, 'w')) 
+  except pickle.PicklingError: 
+    print ("Unpicklable library! NANISORE?") 
+    return 1 
+
+  return 0 
+
+
+# the main routine 
+def main(): 
+  
+  # the library is just a list of dictionaries, one dict per song
+  songs = load_library() 
+
+  if (songs is None): 
+    return 1 
+
+  # try not to mess everything up. keep a backup, just in case. 
+  backup_library() 
+
+  # mutate the songs pickle. 
+  ret = prune_skips(songs) 
+
+  return ret 
+
+
+##### EXECUTION BEGINS HEEEERREEEEE #####
+
+import os, sys 
+try: 
+  import pickle 
+except ImportError: 
+  print ("Error importing pickle! Make sure you have the Python pickle module.")
+  sys.exit(1) 
+
+path_to_songs = os.getenv('HOME') + "/.quodlibet/songs"
+
+ret = main()
+
+sys.exit(ret) 
