8ac47fd825a8f7b86221a0bafd5f7b087711db24
j39m <hikaph@gmail.com>
2015-03-27 23:35:05 -0700

    added automatic pulseaudio redirect: either to HDMI or back to default analogue out

--- tomato_enpassant.sh
+++ tomato_enpassant.sh
@@ -15,9 +15,11 @@ killall compton;
 if [[ "$xrandr_output" =~ "HDMI1 connected" ]]; then 
   printf "Doing stuff for HDMI connection.\n"; 
   xrandr --output HDMI1 --auto --left-of LVDS1; 
+  pactl set-card-profile 0 output:hdmi-stereo+input:analog-stereo; 
 else 
   printf "Doing stuff for HDMI disconnection.\n"; 
   xrandr --auto; 
+  pactl set-card-profile 0 output:analog-stereo+input:analog-stereo; 
 fi; 
 
 { compton > /dev/null 2>&1 & }; 
