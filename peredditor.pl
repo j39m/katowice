8c08a53b71696b85437ef708757f57b1bbf15b38
j39m <hikaph@gmail.com>
2015-07-16 22:18:51 -0700

    additions and expansions

--- peredditor.pl
+++ peredditor.pl
@@ -7,9 +7,17 @@ use strict;
 use warnings; 
 #use feature "switch"; 
 
-# things used internally by peredditor, e.g. current state 
+# things used internally by peredditor, e.g. current state. 
+# implementation of history is that you leave a trail pushed
+# into global_history_behind; when you use "back," it pops 
+# _behind and pushes to _front. "forward" pops from _front and
+# pushes to _behind. At all times your current state is defined
+# by the last element in global_history_behind, which is loaded
+# into global_current_page. 
 my $prompt = "[peredditor] > "; 
-my @global_history = (); 
+my @global_history_behind = (); 
+my @global_history_front = (); 
+my $global_current_page = ""; 
 
 # things read in for control flow 
 my $line = ""; 
@@ -21,11 +29,15 @@ check_prereqs();
 
 print "$prompt"; 
 
+my $err = 0; 
+
 while($line = <STDIN>) { 
   chomp ($line); 
   ($command, $multiplier, $trailing) = parse_command ($line); 
   #print "$command; $multiplier; $aux\n"; 
-  run_command ($command, $multiplier, $trailing); 
+  unless ($multiplier == 0 or $command eq "") { 
+    $err = run_command ($command, $multiplier, $trailing); 
+  } 
   print "$prompt"; 
 } 
 
@@ -79,40 +91,90 @@ sub parse_command {
 
 # takes in the result from parse_command() and acts appropriately.
 # currently ignores the trailing part of the command. 
+# returns 0 on success, nonzero otherwise. 
 sub run_command { 
   my ($com, $mul, $tail) = @_; 
 
-# run_command can be expressed as a given...when loop, but this 
-# is still "experimental." 
-#  given ( $com ) { 
-#    when ("h") { 
-#    } when ("g") { 
-#    } when ("b") {
-#    } when ("f") { 
-#    } when ("fl") { 
-#    } when ("q") { 
-#      tap_out (0); 
-#    } default { 
-#      print STDERR "I don't understand \"$com\".\n"; 
-#      continue; 
-#    } 
-#  } 
+  # run_command can be expressed as a given...when loop, but this 
+  # is still "experimental." 
+  #given ( $com ) { 
+    #when ("h") { 
+    #} when ("g") { 
+    #} when ("b") {
+    #} when ("f") { 
+    #} when ("fl") { 
+    #} when ("q") { 
+      #tap_out (0); 
+    #} default { 
+      #print STDERR "I don't understand \"$com\".\n"; 
+      #continue; 
+    #} 
+  #} 
   if ($com eq "h") { 
+    print_help (); 
   } elsif ($com eq "g") { 
+    go_somewhere ($tail); 
+  } elsif ($com eq "v") { 
+    return 0; # no-op to view current page only. 
   } elsif ($com eq "b") { 
+    return move_back (); 
   } elsif ($com eq "f") { 
+    return move_forward (); 
   } elsif ($com eq "fl") { 
   } elsif ($com eq "q") { 
     tap_out (0); 
   } else { 
     print STDERR "I don't understand \"$com\".\n"; 
+    return 1; 
   } 
 
-  return; 
+  return 0; 
 } 
 
 
-sub fetch_http { 
+# goes somewhere --- as of now, restricted to going to 
+# subreddits. takes the subreddit in the form "r/somesubreddit" 
+# and goes. updates program state to be viewing whatever 
+# subreddit as arg. 
+sub go_somewhere { 
+  my ($sr) = @_; 
+  my $page = fetch_subreddit ($sr); 
+} 
+
+
+# fetches a subreddit totally via curl and returns it as a 
+# string. single argument is of the form "r/somesubreddit." 
+sub fetch_subreddit { 
+  my ($bare_sr) = @_; 
+  my $full_url = "http://reddit.com/" . $bare_sr; 
+  my $ret = `curl -L '$full_url' 2>/dev/null`; 
+  return $ret; 
+} 
+
+
+# goes one page back in history. 
+# returns 0 if success, nonzero if unable. 
+sub move_back { 
+  my $len = @global_history_behind; 
+  if (!$len) { 
+    return 1; 
+  } 
+  my $elem = pop (@global_history_behind); 
+  push (@global_history_front, ($elem)); 
+  return 0; 
+} 
+
+
+# goes one page forward in history. 
+# returns 0 if success, nonzero if unable. 
+sub move_forward { 
+  my $len = @global_history_front; 
+  if (!$len) { 
+    return 1; 
+  } 
+  my $elem = pop (@global_history_front); 
+  push (@global_history_behind, ($elem)); 
+  return 0; 
 } 
 
 
