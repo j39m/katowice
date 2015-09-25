28f17d0effa6dc85777524fb1a6adf9326d0828d
j39m <hikaph@gmail.com>
2015-09-24 23:02:42 -0700

    add silly network simulator

--- chicken_salad.py
+++ chicken_salad.py
@@ -1,4 +1,7 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
+
+import random
+import codecs
 
 # modular exponentiation. 
 
@@ -13,7 +16,6 @@ def expm(base, exponent, mod):
 # remove every other character per line in a text file 
 # of particular ascii art. 
 
-import codecs 
 def sat(filename): 
   fp = codecs.open(filename, "r", encoding = "utf-8")
   out = codecs.open("satsuki-out", "w", encoding = "utf-8")
@@ -30,4 +32,55 @@ def sat(filename):
 
   return 
 
-sat("satsuki")
+#sat("satsuki")
+
+
+class ProbabilisticFailure:
+    """
+    simulates probablistic failure of a linear network
+    """
+
+    def __init__(self, P=.78, N=78, K=13, TEST_TIMES=1300):
+        self.P = P      # probability of failure
+        self.N = N      # number of hops
+        self.K = K      # number of retries in event of failure
+        self.TEST_TIMES = TEST_TIMES # number of tests to run over a lot of hops
+
+    def single_hop(self):
+        retries = self.K
+        while retries:
+            result = random.random()
+            if result > self.P:
+                return True
+            retries -= 1
+        return False
+
+    def many_hops(self):
+        hops = self.N
+        while hops:
+            if not self.single_hop():
+                return False
+            hops -= 1
+        return True
+
+    def many_trials(self):
+        trials = self.TEST_TIMES
+        failures = 0
+        successes = 0
+        while trials:
+            if self.many_hops():
+                successes += 1
+            else:
+                failures += 1
+            trials -= 1
+        return (successes, failures)
+
+    def diagnose(self):
+        (successes, failures) = self.many_trials()
+        expected = (1-(self.P**(self.K)))**self.N
+        msg = str("expected %03f, got %03f"
+                  % (expected, successes/self.TEST_TIMES))
+        print(msg)
+
+klaus = ProbabilisticFailure(.65)
+klaus.diagnose()
