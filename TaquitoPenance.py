55359e1c1342d3750a1ae138e9ea438b70d17a56
j39m <hikaph@gmail.com>
2015-10-27 01:35:41 -0700

    more parsing, some action, almost ready to plot

--- TaquitoPenance.py
+++ TaquitoPenance.py
@@ -5,7 +5,7 @@ Plot my penance.
 """
 
 import datetime
-import matplotlib
+import matplotlib.pyplot as plot
 import re
 
 import os
@@ -66,6 +66,8 @@ class TaquitoPenance(object):
 
     def __init__(self, *args):
 
+        self.name = "TaquitoPenance"
+
         penance = None
         if len(args) > 1:
             penance = args[1]
@@ -73,6 +75,13 @@ class TaquitoPenance(object):
             self.penance = TaquitoReader(PENANCE_FILE)
         else:
             self.penance = TaquitoReader(penance)
+
+    def snark(self, message):
+        """
+        Generic message carrier.
+        """
+        msg = str("%s: %s" % (self.name, str(message)))
+        print(msg)
 
     def parse(self, line):
         """
@@ -89,8 +98,39 @@ class TaquitoPenance(object):
 
     def run(self):
 
+        olddate = None
+        x_axis = []
+        y_axis = []
+        annotations = []
+        running_time = 0
+
         for line in self.penance:
+
             (date, delta, total, comment) = self.parse(line)
+
+            if not olddate:
+                x_axis.append(0)
+            else:
+                time_delta = date - olddate
+                time_delta = time_delta.days
+                if time_delta < 1:
+                    self.snark("bad date bounds (%s, %s)" %(date, olddate))
+                    return 1
+                running_time += time_delta
+                x_axis.append(running_time)
+
+            y_axis.append(total)
+
+            ann = str("%s: %s" % (str(date), comment))
+            annotations.append(ann)
+
+            olddate = date
+
+        print(x_axis)
+        print(y_axis)
+        print(annotations)
+
+        return 0
 
 
 if __name__ == "__main__":
