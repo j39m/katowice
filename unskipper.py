46e8bbc179f10ef7e052b24d0d53462632f9d6a6
j39m <hikaph@gmail.com>
2017-04-01 15:29:05 -0700

    Rework unskipper to be prettier. Trim trailing whitespace. Make operation less safe.

--- unskipper.py
+++ unskipper.py
@@ -1,82 +1,63 @@
-#! /usr/bin/env python2 
+#! /usr/bin/env python2
 
-# unskipper.py will prune all skipcounts from your Quod Libet library; 
+# unskipper.py will prune all skipcounts from your Quod Libet library;
 # the resulting lack of '~#skipcount' in your per-song entries will all
-# be interpreted by QL as being skipcount 0. 
+# be interpreted by QL as being skipcount 0.
 
 
 import os
-import os.path
 import sys
 import shutil
+import pickle
 
-try: 
-  import pickle 
-except ImportError: 
-  print ("Error importing pickle! Make sure you have the Python pickle module.")
-  sys.exit(1) 
+HOME = os.getenv("HOME")
+QLDIR = ".quodlibet"
+PATH_TO_SONGS = os.path.join(
+    HOME,
+    QLDIR,
+    "songs",
+)
+PATH_TO_BKUP = os.path.join(
+    HOME,
+    QLDIR,
+    "unpruned",
+)
 
-path_to_songs = os.getenv('HOME') + "/.quodlibet/songs"
 
-
-# load the pickled library. returns the songs pickle if it can find it, 
-# None if it cannot. 
-def load_library(): 
-  try: 
-    sfh = open(path_to_songs, 'r')
+def load_library():
+    sfh = open(PATH_TO_SONGS, 'r')
     songs = pickle.load(sfh)
     sfh.close()
-  except IOError: 
-    print ("Error loading songs pickle. Does '%s' exist?" % path_to_songs)
-    return None 
+    return songs
 
-  return songs
+def backup_library():
+    sfh = open(PATH_TO_SONGS, "rb")
+    bfh = open(PATH_TO_BKUP, "wb")
+    shutil.copyfileobj(sfh, bfh)
+    sfh.close()
+    bfh.close()
+    return 0
 
-
-# in case we muck things up, have a backup on hand 
-def backup_library(): 
-
-  path_to_bkup =  os.getenv('HOME') + "/.quodlibet/unpruned_songs"
-
-  if os.path.isfile(path_to_bkup):
-    print ("Couldn't backup current library state. Aborting.") 
-    print ("Please remove '%s' before trying again." % path_to_bkup) 
-    return False 
-
-  sfh = open(path_to_songs, "rb")
-  bfh = open(path_to_bkup, "wb")
-  shutil.copyfileobj(sfh, bfh)
-  sfh.close()
-  bfh.close()
-
-  print ("Current library state copied to %s" % path_to_bkup) 
-  return True 
-
-
-# weed out skipcounts wherever they may be
-def prune_skips(song_pickle): 
-  
-  found_skips = False # make me true if we need to prune anything
-
-  for song in song_pickle: 
-    if ('~#skipcount' in song): 
-      found_skips = True
-      try: 
-        print ("prune: %d skips on '%s'" % (song.pop('~#skipcount'), song['title'] ) )
-      except KeyError:  # if song is untitled???
-        print ("prune: %d skips on untitled song" % song.pop('~#skipcount') )
-
-  if (not found_skips): 
-    print ("No skips pruned.") 
-
-  # write the finished pickle down 
-  try: 
-    pickle.dump(song_pickle, open(path_to_songs, 'w')) 
-  except pickle.PicklingError: 
-    print ("Unpicklable library! NANISORE?") 
-    return 1 
-
-  return 0 
+def prune_skips(song_pickle):
+    """Main function for pruning skips from a pickle."""
+    found_skips = False
+    for song in song_pickle:
+        try:
+            skipmsg = str(
+                "prune %d skips on ``%s''."
+                % (song.pop("~#skipcount"), song["title"])
+            )
+            found_skips = True
+            print(skipmsg)
+        except KeyError:
+            continue
+    # write the finished pickle down
+    try:
+        pickle.dump(song_pickle, open(PATH_TO_SONGS, "w"))
+    except pickle.PicklingError:
+        print ("NANISORE?")
+        return 1
+    return 0
 
 
 def query_library_by_tag(lib, val, tag="artist", corr="~#playcount", rkey="title"):
@@ -85,7 +66,6 @@ def query_library_by_tag(lib, val, tag="artist", corr="~#playcount", rkey="title
     value "val."
     returns a dictionary of the results.
     """
-
     retv = {}
     for song in lib:
         if tag in song and val in song[tag] and corr in song:
@@ -99,30 +79,18 @@ def query_library_by_tag(lib, val, tag="artist", corr="~#playcount", rkey="title
                 retv[lkey].append(song[corr])
             else:
                 retv[lkey] = song[corr]
-
     return retv
 
-# the main routine 
-def main(): 
-  
-  # the library is just a list of dictionaries, one dict per song
-  songs = load_library() 
+def main():
+    """The main entry point."""
+    songs = load_library()
+    backup_library()
 
-  if songs is None: 
-    return 1 
-
-  # try not to mess everything up. keep a backup, just in case. 
-  if not backup_library(): 
-    return 1 
-
-  # mutate the songs pickle. 
-  ret = prune_skips(songs) 
-
-  return ret 
+    return prune_skips(songs)
 
 
 ##### EXECUTION BEGINS HEEEERREEEEE #####
 
-if __name__ == "__main__": 
-  ret = main()
-  sys.exit(ret) 
+if __name__ == "__main__":
+    ret = main()
+    sys.exit(ret)
