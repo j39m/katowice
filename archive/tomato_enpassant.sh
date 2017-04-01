fc35ee1804e0a3e65807af5dd35aaf52a5bddc7d
j39m <hikaph@gmail.com>
2017-04-01 14:45:12 -0700

    Update to current hardware configuration.

--- archive/tomato_enpassant.sh
+++ archive/tomato_enpassant.sh
@@ -5,19 +5,20 @@
 
 # stopgap for feh using a relative path for its wallpaper.
 # The below should just reflect whatever directory makes
-# the wallpaper path coherent. 
-cd ~; 
+# the wallpaper path coherent.
+cd ~;
 
-xrandr_output=$(xrandr -q); 
+xrandr_output=$(xrandr -q);
 
-if [[ "$xrandr_output" =~ "HDMI1 connected" ]]; then 
-  printf "Doing stuff for HDMI connection.\n"; 
-  xrandr --output HDMI1 --auto --left-of eDP1; 
-  pactl set-card-profile alsa_card.pci-0000_00_03.0 output:hdmi-stereo ; 
-else 
-  printf "Doing stuff for HDMI disconnection.\n"; 
-  xrandr --auto; 
-  pactl set-card-profile alsa_card.pci-0000_00_1b.0 output:analog-stereo ;
-fi; 
+if [[ "$xrandr_output" =~ "HDMI-1 connected" ]]; then
+  printf "Doing stuff for HDMI connection.\n";
+  xrandr --output HDMI-1 --primary --auto --left-of eDP-1 \
+    --output eDP-1 --auto;
+  #pactl set-card-profile alsa_card.pci-0000_00_03.0 output:hdmi-stereo ;
+else
+  printf "Doing stuff for HDMI disconnection.\n";
+  xrandr --auto;
+  #pactl set-card-profile alsa_card.pci-0000_00_1b.0 output:analog-stereo ;
+fi;
 
-sh ~/.fehbg; 
+sh ~/.fehbg;
