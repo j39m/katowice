da9f3ee7c0dffaf3086ee30b9bf27becfaa0d800
j39m <hikaph@gmail.com>
2019-04-17 22:52:40 -0700

    nanisore: discard Perl.
    There's no good reason to keep this in Perl; this change rewrites
    nanisore to be sourced as a series of bash functions.

---
+++ archive/nanisore.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+NANISORE_WIDTH=52
+NANISORE_SCREENFUL=39
+
+function _nanisore() {
+    ps -eo args:"${NANISORE_WIDTH}",euser,pid,pcpu,pmem,state \
+        --sort="${1}"
+}
+
+function nanisore() {
+    _nanisore -rss | head -n "${NANISORE_SCREENFUL}"
+}
+
+function hahisore() {
+    _nanisore state
+}
+
+function mamisore() {
+    _nanisore -pcpu | head -n "${NANISORE_SCREENFUL}"
+}
