115d3fe40b0fad108da23a5a8a53c1b9e28349a0
j39m <hikaph@gmail.com>
2020-02-06 01:06:56 -0800

    xkpw: complete Rust port
    This change brings the Rust port of xkpw to feature completion.

--- xkpw/main.rs
+++ xkpw/main.rs
@@ -20,11 +20,10 @@ pub enum PasswordOptions {
 }
 
 mod args {
-    use crate::{EnglishPasswordOptions, KanaPasswordOptions, PasswordOptions};
     use clap::{value_t, values_t};
 
     // Aborts this process on error.
-    pub fn parse_args() -> PasswordOptions {
+    pub fn parse_args() -> crate::PasswordOptions {
         let matches = clap::App::new("xkpw")
             .version("0.1.0")
             .author("j39m")
@@ -67,6 +66,51 @@ mod args {
     }
 } // mod args
 
+mod kana {
+    pub const BASE: &'static [&str] = &[
+        "a", "i", "u", "e", "o", "ka", "ki", "ku", "ke", "ko", "sa", "shi", "su", "se", "so", "ta",
+        "chi", "tsu", "te", "to", "na", "ni", "nu", "ne", "no", "ha", "hi", "hu", "he", "ho", "ma",
+        "mi", "mu", "me", "mo", "ya", "yu", "yo", "ra", "ri", "ru", "re", "ro", "wa", "wo",
+    ];
+
+    pub const DIGRAPHS: &'static [&str] = &[
+        "kya", "kyu", "kyo", "sha", "shu", "sho", "cha", "chu", "cho", "nya", "nyu", "nyo", "hya",
+        "hyu", "hyo", "mya", "myu", "myo", "rya", "ryu", "ryo",
+    ];
+
+    pub const DIACRITICS: &'static [&str] = &[
+        "ga", "gi", "gu", "ge", "go", "za", "ji", "zu", "ze", "zo", "da", "de", "do", "ba", "bi",
+        "bu", "be", "bo", "pa", "pi", "pu", "pe", "po",
+    ];
+
+    pub const DIGRAPHS_WITH_DIACRITICS: &'static [&str] = &[
+        "gya", "gyu", "gyo", "ja", "ju", "jo", "bya", "byu", "byo", "pya", "pyu", "pyo",
+    ];
+
+    fn all() -> Vec<&'static str> {
+        let mut all_kana: Vec<&'static str> = Vec::new();
+        all_kana.extend(BASE);
+        all_kana.extend(DIGRAPHS);
+        all_kana.extend(DIACRITICS);
+        all_kana.extend(DIGRAPHS_WITH_DIACRITICS);
+        all_kana
+    }
+
+    fn simple() -> Vec<&'static str> {
+        let mut simple_kana: Vec<&'static str> = Vec::new();
+        simple_kana.extend(BASE);
+        simple_kana.extend(DIACRITICS);
+        simple_kana
+    }
+
+    pub fn get(use_simple: bool) -> Vec<&'static str> {
+        match use_simple {
+            true => simple(),
+            false => all(),
+        }
+    }
+} // mod kana
+
 mod helpers {
     use rand::seq::SliceRandom;
     use std::convert::TryInto;
@@ -84,22 +128,10 @@ mod helpers {
             .collect())
     }
 
-    // Returns 1 if we fail to read the word dictionary.
-    fn print_english_password(options: crate::EnglishPasswordOptions) -> i32 {
-        let words = match ingest_english_dictionary(std::path::Path::new(&options.dictionary_path))
-        {
-            Ok(result) => result,
-            Err(error) => {
-                eprintln!("Error reading dictionary: {}", error);
-                return 1;
-            }
-        };
-        let mut rng = &mut rand::thread_rng();
-        let selection: Vec<&String> = words
-            .choose_multiple(&mut rng, options.num_words.try_into().unwrap())
-            .collect();
-
-        let mut iter = selection.iter();
+    // Assumes ownership of a Vector of owned |words| and prints them
+    // separated by spaces.
+    fn print_words(words: Vec<String>) {
+        let mut iter = words.into_iter();
         if let Some(word) = iter.next() {
             print!("{}", word);
             for word in iter {
@@ -107,11 +139,55 @@ mod helpers {
             }
             print!("\n");
         }
+    }
+
+    // Returns 1 if we fail to read the word dictionary.
+    fn print_english_password(options: crate::EnglishPasswordOptions) -> i32 {
+        let all_words =
+            match ingest_english_dictionary(std::path::Path::new(&options.dictionary_path)) {
+                Ok(result) => result,
+                Err(error) => {
+                    eprintln!("Error reading dictionary: {}", error);
+                    return 1;
+                }
+            };
+        let mut rng = &mut rand::thread_rng();
+        let selection: Vec<String> = all_words
+            .choose_multiple(&mut rng, options.num_words.try_into().unwrap())
+            .map(|borrowed| borrowed.to_owned())
+            .collect();
+
+        print_words(selection);
         0
     }
 
+    // Builds a single random pseudo-Japanese word from |kana_set|.
+    fn build_kana_word(kana_set: &Vec<&'static str>, syllable_count: &u8) -> String {
+        let mut collected_kana: Vec<&'static str> = Vec::new();
+        let mut rng = &mut rand::thread_rng();
+        for _ in 0..*syllable_count {
+            collected_kana.push(kana_set.choose(&mut rng).unwrap());
+        }
+        collected_kana.join("")
+    }
+
+    // Builds random pseudo-Japanese words from |kana_set|, observing
+    // the word lengths specified by |options|.
+    fn build_kana_words(
+        kana_set: Vec<&'static str>,
+        options: crate::KanaPasswordOptions,
+    ) -> Vec<String> {
+        options
+            .syllable_counts
+            .iter()
+            .map(|count| build_kana_word(&kana_set, count))
+            .collect()
+    }
+
+    // Builds a vector of owned kana words, maps these to a vector of
+    // borrowed kana words, and then prints the same.
     fn print_kana_password(options: crate::KanaPasswordOptions) {
-        eprintln!("XXX(j39m): FIX ME");
+        print_words(build_kana_words(crate::kana::get(true), options));
     }
 
     pub fn main() -> i32 {
