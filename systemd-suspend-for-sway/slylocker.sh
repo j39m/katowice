ffc78b0cbae8eb3c0c88d183de2ddf709cdbac34
j39m <hikaph@gmail.com>
2020-10-24 00:56:54 -0700

    slylocker: block in ExecStart
    This change
    * modifies the suspend unit to suspend immediately after the slylocker
      script exits and
    * modifies slylocker.sh to block (a while) waiting for swaylock to
      start.

--- systemd-suspend-for-sway/slylocker.sh
+++ systemd-suspend-for-sway/slylocker.sh
@@ -5,6 +5,11 @@ set -eu;
 uid="$(id -u)";
 
 for sock in /run/user/"${uid}"/sway-ipc.*.*.sock; do
-    [ -S "${sock}" ] || continue;
-    swaymsg -s "${sock}" exec -- /usr/bin/swaylock;
-done;
+    [ -S "${sock}" ] || continue
+    swaymsg -s "${sock}" exec -- /usr/bin/swaylock
+done
+
+for _ in {1..26}; do
+    pgrep -u "${uid}" -x swaylock && break
+    sleep 0.13
+done
