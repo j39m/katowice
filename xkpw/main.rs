c960f5fdfd4a6028f8c8a9b4c6c952b720e9cb10
j39m <hikaph+oss@gmail.com>
2022-03-10 21:06:03 -0800

    xkpw: Reformat hiragana arrays

--- xkpw/main.rs
+++ xkpw/main.rs
@@ -72,24 +72,46 @@ mod args {
 } // mod args
 
 mod kana {
+    #[rustfmt::skip]
     const BASE: &'static [&str] = &[
-        "a", "i", "u", "e", "o", "ka", "ki", "ku", "ke", "ko", "sa", "shi", "su", "se", "so", "ta",
-        "chi", "tsu", "te", "to", "na", "ni", "nu", "ne", "no", "ha", "hi", "hu", "he", "ho", "ma",
-        "mi", "mu", "me", "mo", "ya", "yu", "yo", "ra", "ri", "ru", "re", "ro", "wa", "wo",
+        "a",    "i",    "u",    "e",    "o",
+        "ka",   "ki",   "ku",   "ke",   "ko",
+        "sa",   "shi",  "su",   "se",   "so",
+        "ta",   "chi",  "tsu",  "te",   "to",
+        "na",   "ni",   "nu",   "ne",   "no",
+        "ha",   "hi",   "hu",   "he",   "ho",
+        "ma",   "mi",   "mu",   "me",   "mo",
+        "ya",           "yu",           "yo",  
+        "ra",   "ri",   "ru",   "re",   "ro",
+        "wa",                           "wo",
     ];
 
+    #[rustfmt::skip]
     const DIGRAPHS: &'static [&str] = &[
-        "kya", "kyu", "kyo", "sha", "shu", "sho", "cha", "chu", "cho", "nya", "nyu", "nyo", "hya",
-        "hyu", "hyo", "mya", "myu", "myo", "rya", "ryu", "ryo",
+        "kya", "kyu", "kyo",
+        "sha", "shu", "sho",
+        "cha", "chu", "cho",
+        "nya", "nyu", "nyo",
+        "hya", "hyu", "hyo",
+        "mya", "myu", "myo",
+        "rya", "ryu", "ryo",
     ];
 
+    #[rustfmt::skip]
     const DIACRITICS: &'static [&str] = &[
-        "ga", "gi", "gu", "ge", "go", "za", "ji", "zu", "ze", "zo", "da", "de", "do", "ba", "bi",
-        "bu", "be", "bo", "pa", "pi", "pu", "pe", "po",
+        "ga", "gi", "gu", "ge", "go",
+        "za", "ji", "zu", "ze", "zo",
+        "da",             "de", "do",
+        "ba", "bi", "bu", "be", "bo",
+        "pa", "pi", "pu", "pe", "po",
     ];
 
+    #[rustfmt::skip]
     const DIGRAPHS_WITH_DIACRITICS: &'static [&str] = &[
-        "gya", "gyu", "gyo", "ja", "ju", "jo", "bya", "byu", "byo", "pya", "pyu", "pyo",
+        "gya",  "gyu",  "gyo",
+        "ja",   "ju",   "jo",
+        "bya",  "byu",  "byo",
+        "pya",  "pyu",  "pyo",
     ];
 
     fn all() -> Vec<&'static str> {
