1c62e87f4b35c4c8123a4ec6888c765a20cc6fd8
j39m <hikaph+oss@gmail.com>
2024-05-13 23:57:05 +0900

    xkpw: Roll to clap 4

--- xkpw/main.rs
+++ xkpw/main.rs
@@ -1,82 +1,42 @@
 extern crate clap;
 extern crate rand;
 
-pub const DEFAULT_ENGLISH_DICTIONARY_PATH: &'static str = "/usr/share/dict/words";
+const DEFAULT_ENGLISH_DICTIONARY_PATH: &'static str = "/usr/share/dict/words";
 
-#[derive(Debug)]
-pub struct EnglishPasswordOptions {
-    pub num_words: u8,
-    pub dictionary_path: String,
+use clap::Parser;
+
+#[derive(clap::Parser)]
+#[command(name = clap::crate_name!(), version = clap::crate_version!())]
+struct Cli {
+    #[command(subcommand)]
+    subcommand: Option<Subcommand>,
 }
 
-#[derive(Debug)]
-pub struct KanaPasswordOptions {
-    pub syllable_counts: Vec<u8>,
-    pub use_all_hiragana: bool,
+#[derive(clap::Subcommand)]
+enum Subcommand {
+    /// create English-ish password
+    En(EnglishArgs),
+    /// create faux-Japanese password
+    Jp(JapaneseArgs),
 }
 
-pub enum PasswordOptions {
-    English(EnglishPasswordOptions),
-    Japanese(KanaPasswordOptions),
+#[derive(clap::Args)]
+struct EnglishArgs {
+    #[arg(short, default_value_t =
+          String::from(DEFAULT_ENGLISH_DICTIONARY_PATH),
+          help = "dictionary path")]
+    dictionary: String,
+    #[arg(help = "word count")]
+    count: usize,
 }
 
-mod args {
-    use super::{
-        EnglishPasswordOptions, KanaPasswordOptions, PasswordOptions,
-        DEFAULT_ENGLISH_DICTIONARY_PATH,
-    };
-
-    // Aborts this process on error.
-    pub fn parse_args() -> PasswordOptions {
-        let matches = clap::App::new("xkpw")
-            .version("0.2.1")
-            .author("j39m")
-            .about("Generates passwords")
-            .subcommand(
-                clap::App::new("en")
-                    .about("generates dictionary passwords in English")
-                    .arg(clap::Arg::new("num-words").required(true)),
-            )
-            .subcommand(
-                clap::App::new("jp")
-                    .about("generates random strings of Japanese syllables")
-                    .arg(clap::Arg::new("use-all-hiragana").short('a').long("harder"))
-                    .arg(
-                        clap::Arg::new("syllable-counts")
-                            .required(true)
-                            .short('c')
-                            .long("syllable-counts")
-                            .takes_value(true)
-                            .multiple_values(true),
-                    ),
-            )
-            .get_matches();
-
-        match matches.subcommand() {
-            Some(("en", en_matches)) => {
-                return PasswordOptions::English(EnglishPasswordOptions {
-                    num_words: en_matches
-                        .value_of_t::<u8>("num-words")
-                        .unwrap_or_else(|e| e.exit()),
-                    dictionary_path: DEFAULT_ENGLISH_DICTIONARY_PATH.to_owned(),
-                })
-            }
-            Some(("jp", jp_matches)) => {
-                return PasswordOptions::Japanese(KanaPasswordOptions {
-                    syllable_counts: jp_matches
-                        .values_of_t("syllable-counts")
-                        .unwrap_or_else(|e| e.exit()),
-                    use_all_hiragana: jp_matches.is_present("use-all-hiragana"),
-                })
-            }
-            _ => (),
-        }
-        PasswordOptions::Japanese(KanaPasswordOptions {
-            syllable_counts: vec![4, 3, 3, 3],
-            use_all_hiragana: true,
-        })
-    }
-} // mod args
+#[derive(clap::Args)]
+struct JapaneseArgs {
+    #[arg(short, default_value_t = false, help = "also use harder kana")]
+    all_kana: bool,
+    #[arg(default_values_t = vec![4, 3, 3, 3], help = "syllable counts")]
+    counts: Vec<usize>,
+}
 
 mod kana {
     #[rustfmt::skip]
@@ -145,96 +105,81 @@ mod kana {
     }
 } // mod kana
 
-mod helpers {
-    use super::{EnglishPasswordOptions, KanaPasswordOptions, PasswordOptions};
-    use rand::seq::SliceRandom;
-    use std::convert::TryInto;
-    use std::io::Read;
+use rand::seq::SliceRandom;
+use std::io::Read;
 
-    // Reads the dictionary at |path| and returns a linewise vector of
-    // its contents.
-    fn ingest_english_dictionary(path: &std::path::Path) -> std::io::Result<Vec<String>> {
-        let mut file = std::fs::File::open(path)?;
-        let mut contents = String::new();
-        file.read_to_string(&mut contents)?;
-        Ok(contents
-            .lines()
-            .map(|word| word.trim().to_lowercase())
-            .collect())
-    }
+// Reads the dictionary at |path| and returns a linewise vector of
+// its contents.
+fn ingest_english_dictionary(path: &std::path::Path) -> std::io::Result<Vec<String>> {
+    let mut file = std::fs::File::open(path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents
+        .lines()
+        .map(|word| word.trim().to_lowercase())
+        .collect())
+}
 
-    // Assumes ownership of a Vector of owned |words| and prints them
-    // separated by spaces.
-    fn print_words(words: Vec<String>) {
-        let mut iter = words.into_iter();
-        if let Some(word) = iter.next() {
-            print!("{}", word);
-            for word in iter {
-                print!(" {}", word);
-            }
-            print!("\n");
+// Assumes ownership of a Vector of owned |words| and prints them
+// separated by spaces.
+fn print_words(words: Vec<String>) {
+    let mut iter = words.into_iter();
+    if let Some(word) = iter.next() {
+        print!("{}", word);
+        for word in iter {
+            print!(" {}", word);
         }
+        print!("\n");
     }
+}
 
-    // Returns 1 if we fail to read the word dictionary.
-    fn print_english_password(options: EnglishPasswordOptions) -> i32 {
-        let all_words =
-            match ingest_english_dictionary(std::path::Path::new(&options.dictionary_path)) {
-                Ok(result) => result,
-                Err(error) => {
-                    eprintln!("Error reading dictionary: {}", error);
-                    return 1;
-                }
-            };
-        let mut rng = &mut rand::thread_rng();
-        let selection: Vec<String> = all_words
-            .choose_multiple(&mut rng, options.num_words.try_into().unwrap())
-            .map(|borrowed| borrowed.to_owned())
-            .collect();
-
-        print_words(selection);
-        0
-    }
-
-    // Builds a single random pseudo-Japanese word from |kana_set|.
-    fn build_kana_word(kana_set: &Vec<&'static str>, syllable_count: &u8) -> String {
-        let mut collected_kana: Vec<&'static str> = Vec::new();
-        let mut rng = &mut rand::thread_rng();
-        for _ in 0..*syllable_count {
-            collected_kana.push(kana_set.choose(&mut rng).unwrap());
+// Returns 1 if we fail to read the word dictionary.
+fn print_english_password(args: EnglishArgs) {
+    let all_words = match ingest_english_dictionary(std::path::Path::new(&args.dictionary)) {
+        Ok(result) => result,
+        Err(error) => {
+            panic!("Error reading dictionary: {}", error);
         }
-        collected_kana.join("")
-    }
+    };
+    let mut rng = &mut rand::thread_rng();
+    let selection: Vec<String> = all_words
+        .choose_multiple(&mut rng, args.count)
+        .map(|borrowed| borrowed.to_owned())
+        .collect();
 
-    // Builds random pseudo-Japanese words from |kana_set|, observing
-    // the word lengths specified by |options|.
-    fn build_kana_words(kana_set: Vec<&'static str>, options: KanaPasswordOptions) -> Vec<String> {
-        options
-            .syllable_counts
-            .iter()
-            .map(|count| build_kana_word(&kana_set, count))
-            .collect()
-    }
+    print_words(selection);
+}
 
-    // Builds a vector of owned kana words, maps these to a vector of
-    // borrowed kana words, and then prints the same.
-    fn print_kana_password(options: KanaPasswordOptions) {
-        print_words(build_kana_words(
-            crate::kana::get(options.use_all_hiragana),
-            options,
-        ));
+// Builds a single random pseudo-Japanese word from |kana_set|.
+fn build_kana_word(kana_set: &Vec<&'static str>, syllable_count: usize) -> String {
+    let mut collected_kana: Vec<&'static str> = Vec::new();
+    let mut rng = &mut rand::thread_rng();
+    for _ in 0..syllable_count {
+        collected_kana.push(kana_set.choose(&mut rng).unwrap());
     }
+    collected_kana.join("")
+}
 
-    pub fn main() -> i32 {
-        let options = crate::args::parse_args();
-        match options {
-            PasswordOptions::English(options) => return print_english_password(options),
-            PasswordOptions::Japanese(options) => print_kana_password(options),
-        }
-        0
-    }
-} // mod helpers
+// Builds a vector of owned kana words, maps these to a vector of
+// borrowed kana words, and then prints the same.
+fn print_kana_password(args: JapaneseArgs) {
+    let syllable_set = crate::kana::get(args.all_kana);
+    let words = args
+        .counts
+        .into_iter()
+        .map(|morae_count| build_kana_word(&syllable_set, morae_count))
+        .collect();
+    print_words(words);
+}
 
 fn main() {
-    std::process::exit(crate::helpers::main());
+    let cli = Cli::parse();
+    match cli.subcommand {
+        Some(Subcommand::En(args)) => print_english_password(args),
+        Some(Subcommand::Jp(args)) => print_kana_password(args),
+        None => print_kana_password(JapaneseArgs {
+            all_kana: true,
+            counts: vec![4, 3, 3, 3],
+        }),
+    };
 }
