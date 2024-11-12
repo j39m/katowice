28522250f8ff243f9bbfe498f00e5357b8af17d6
j39m <hikaph+oss@gmail.com>
2024-11-13 00:59:44 +0900

    qlvandal: Use `util` func uniformly

--- qlvandal/poison.py
+++ qlvandal/poison.py
@@ -1,11 +1,9 @@
 import sys
 
-import quodlibet.cli
-
-from . import unskipper
+from . import util
 
 def main():
-    if quodlibet.cli.is_running():
+    if util.quodlibet_is_running():
         print("Detected running Quod Libet - bailing!")
         return 1
     return 0
