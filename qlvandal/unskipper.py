119322e9eaf7e10220fa35ca76c9bb2006627301
j39m <hikaph+oss@gmail.com>
2024-11-13 03:12:31 +0900

    qlvandal: Use a context manager
    Much the same logic will be used in `poison`, so we might as well get it
    nice and shareable now.

--- qlvandal/unskipper.py
+++ qlvandal/unskipper.py
@@ -29,22 +29,12 @@ def main():
     """The main entry point."""
     roll_d20 = random.randint(1, 20)
     if roll_d20 < 15:
-        print(f"Roll {roll_d20} < 15: doing nothing.")
+        print(f"Roll {roll_d20} < 15: doing nothing")
         return 0
 
-    util.backup_library()
-    songs = util.load_library()
-
-    skipped = prune_skips(songs)
-    if not skipped:
-        print("No skips pruned.")
-        return 0
-
-    _print_skips(skipped)
-
-    if util.quodlibet_is_running():
-        print("Detected running Quod Libet - bailing!")
-        return 1
-
-    util.save_library(songs)
+    with util.SongsContextManager() as songs:
+        skipped = prune_skips(songs)
+        if not skipped:
+            raise util.DontSaveLibrary("no skips pruned")
+        _print_skips(skipped)
     return 0
--- qlvandal/util.py
+++ qlvandal/util.py
@@ -3,6 +3,7 @@ import shutil
 import sys
 
 import gi
+
 gi.require_version("PangoCairo", "1.0")
 import quodlibet
 import quodlibet.cli
@@ -16,20 +17,41 @@ def quodlibet_is_running():
     return quodlibet.cli.is_running()
 
 
-def backup_library():
-    shutil.copy2(SONGS_PATH, BKUP_PATH)
+class DontSaveLibrary(Exception):
+    """
+    A nonfatal exception that signals the `SongsContextManager` not to
+    save the library and to swallow this exception, allowing control
+    flow to continue.
+    """
+
+    def __init__(self, reason):
+        self.reason = reason
 
 
-def load_library():
-    sys.modules.pop("gi.repository.Gtk")
-    quodlibet.init()
-    return quodlibet.library.init(SONGS_PATH)
+class SongsContextManager:
 
+    def __init__(self):
+        sys.modules.pop("gi.repository.Gtk")
+        quodlibet.init()
+        self.songs = quodlibet.library.init(SONGS_PATH)
 
-def save_library(songs):
-    tmppath = SONGS_PATH.with_suffix(".tmp")
-    songs.save(tmppath)
-    tmppath.rename(SONGS_PATH)
+    def __enter__(self):
+        return self.songs
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type == DontSaveLibrary:
+            print(f"Not saving library: {exc_value.reason}")
+            return True
+        if quodlibet_is_running():
+            raise ConnectionError("Quod Libet is running")
+        if exc_type == exc_value == traceback == None:
+            shutil.copy2(SONGS_PATH, BKUP_PATH)
+            # Actually reading `save()`, this whole dance appears to be
+            # redundant (ノ°益°)ノ
+            tmppath = SONGS_PATH.with_suffix(".qlvandal")
+            # Hmm. `atomic.py` seems to demand a string-like argument.
+            self.songs.save(str(tmppath))
+            tmppath.rename(SONGS_PATH)
 
 
 def _query_callable(songs, tag, val_callable):
