c983eb2f7662d00de61ec6dcaec510348bdf2fc9
j39m <hikaph+oss@gmail.com>
2025-02-08 10:04:31 +0900

    watch_and_rename: Initial commit

---
+++ watch_and_rename.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+import sys
+import argparse
+import pathlib
+import pyinotify
+
+CWD = "./"
+
+
+class EventHandler(pyinotify.ProcessEvent):
+    # The defaults are actually set by `parse_args()`, but are retained
+    # here to illustrate good defaults.
+    def __init__(self, initial_index=1, prefix=""):
+        self.index = initial_index
+        self.prefix = prefix
+
+    def target_filename(self, suffix):
+        target = pathlib.Path(f"{self.prefix}{self.index:03d}{suffix}")
+        assert not target.is_file()
+        self.index += 1
+        return target
+
+    def process_IN_CREATE(self, event):
+        name = pathlib.Path(event.name)
+        assert name.is_file()
+        target = self.target_filename(name.suffix)
+        name.rename(target)
+        print(f"{target} <- {name}")
+
+
+def _deduce_initial_index():
+    files = [f for f in pathlib.Path(CWD).iterdir() if f.is_file()]
+    return len(files) + 1
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i",
+                        "--initial-index",
+                        default=_deduce_initial_index(),
+                        type=int)
+    parser.add_argument("-p", "--prefix", default="")
+    args = parser.parse_args()
+
+    return EventHandler(args.initial_index, args.prefix)
+
+
+def main():
+    event_handler = parse_args()
+    watch_manager = pyinotify.WatchManager()
+
+    notifier = pyinotify.Notifier(watch_manager, event_handler)
+    watch_manager.add_watch(CWD, pyinotify.IN_CREATE)
+
+    notifier.loop()
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
