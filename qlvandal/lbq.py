6b23cdd7f9a47776f564bfe60841201220d01b10
j39m <hikaph+oss@gmail.com>
2026-07-11 14:04:13 +0900

    lbq: Add bassoon concerti handling

--- qlvandal/lbq.py
+++ qlvandal/lbq.py
@@ -4,24 +4,41 @@ from pprint import pprint
 from . import util
 
 
+def Query(songs, query, filter_regex):
+    titles = [s["title"] for s in songs.query(query)]
+    return sorted(set([int(filter_regex.match(t).group(1)) for t in titles]))
+
+
 def QueryCantatas(songs):
     TARGET_PLAYCOUNT = 7
-    titles = [
-        s["title"]
-        for s in songs.query(
-            f"&(discsubtitle=cantatas, #(playcount<{TARGET_PLAYCOUNT}))"
+    pprint(
+        Query(
+            songs,
+            f"&(discsubtitle=cantatas, #(playcount<{TARGET_PLAYCOUNT}))",
+            re.compile(r".+ BWV ([^\s]+) .+"),
         )
-    ]
-    PATTERN = re.compile(r".+ BWV ([^\s]+) .+")
-    bwvs = sorted(set([int(PATTERN.match(t).group(1)) for t in titles]))
-    pprint(bwvs)
+    )
+
+
+def QueryBassoonConcerti(songs):
+    TARGET_PLAYCOUNT = 6
+    pprint(
+        Query(
+            songs,
+            f"&(artist='Daniel Smith', #(playcount<{TARGET_PLAYCOUNT}))",
+            re.compile(r".+ RV ([^\s]+) .+"),
+        )
+    )
 
 
 def main():
     action_text = sys.argv[1]
     action_func = None
-    if action_text == "c":
+    if action_text.startswith("c"):
         action_func = QueryCantatas
+    elif action_text.startswith("b"):
+        action_func = QueryBassoonConcerti
+
     if action_func is None:
         return 1
     try:
